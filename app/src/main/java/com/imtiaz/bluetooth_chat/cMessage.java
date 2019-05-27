bf372003e60463b3756715f72e2248857c0a1483
imtiazice <mahrabimtiaz@gmail.com>
2019-05-27 21:28:04 +0600

    Bluetooth Chat Application

---
+++ app/src/main/java/com/imtiaz/bluetooth_chat/cMessage.java
@@ -0,0 +1,41 @@
+package com.imtiaz.bluetooth_chat;
+
+public class cMessage {
+    int Me;//=1 if the message was mine and =0 if the message is not mine
+    String Msg;// the message
+    long Recu;// the time message was sent
+    String Adresse;// the address of the target
+
+
+    public cMessage(long recu, String msg, int me, String adresse) {
+        Recu = recu;
+        Msg = msg;
+        Me = me;
+        Adresse = adresse;
+    }
+
+    public int isMe() {
+        return Me;
+    }
+    public void setMe(int me) {
+        Me = me;
+    }
+    public String getMsg() {
+        return Msg;
+    }
+    public void setMsg(String msg) {
+        Msg = msg;
+    }
+    public String getAdresse() {
+        return Adresse;
+    }
+    public void setAdresse(String adresse) {
+        Adresse = adresse;
+    }
+    public long getRecu() {
+        return Recu;
+    }
+    public void setRecu(long recu) {
+        Recu = recu;
+    }
+}
